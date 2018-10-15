b81e736b34d2e108ef09fe9e7ec0e68eddb9ee71
Emerson Hsieh <emersonhsieh@berkeley.edu>
2018-10-15 14:53:46 -0700

    Change placements

--- quilt/placements1.js
+++ quilt/placements1.js
@@ -5,7 +5,6 @@ function placements(hotrod, diskSizes) {
     this.three_per = function three_per() { 
         assert(diskSizes.length == 5);
 
-        hotrod.ingress.placeOn({diskSize: diskSizes[0]});
         hotrod.mapper[0].placeOn({diskSize: diskSizes[0]});
         // 46.0
         hotrod.mapper[1].placeOn({diskSize: diskSizes[0]});
@@ -13,48 +12,48 @@ function placements(hotrod, diskSizes) {
         hotrod.mapper[2].placeOn({diskSize: diskSizes[0]});
         // 46.0
 
-
         hotrod.driver[0].placeOn({diskSize: diskSizes[1]});
         // 34.3
-        hotrod.driver[1].placeOn({diskSize: diskSizes[1]});
-        // 34.3
+        hotrod.route[0].placeOn({diskSize: diskSizes[4]});
+        // 46.0
         hotrod.frontend.placeOn({diskSize: diskSizes[1]});
         // 3.86561
         hotrod.customer[0].placeOn({diskSize: diskSizes[1]});
         // 3.773
 
-        hotrod.driver[2].placeOn({diskSize: diskSizes[2]});
-        // 34.3
+        hotrod.route[1].placeOn({diskSize: diskSizes[4]});
+        // 46.0
         hotrod.customer[1].placeOn({diskSize: diskSizes[2]});
         // 3.773
-        hotrod.customer[2].placeOn({diskSize: diskSizes[2]});
-        // 3.773
-        hotrod.api_haproxy.placeOn({diskSize: diskSizes[2]});
-        // 5.488
-
-        hotrod.map_haproxy.placeOn({diskSize: diskSizes[3]});
-        // 5.488
-        hotrod.api[0].placeOn({diskSize: diskSizes[3]});
-        // 70.0
         hotrod.api[1].placeOn({diskSize: diskSizes[3]});
+        // 70.0
+
+        hotrod.api[0].placeOn({diskSize: diskSizes[3]});
         // 70.0
         hotrod.postgres.placeOn({diskSize: diskSizes[3]});
         // 15.68
+        hotrod.driver[1].placeOn({diskSize: diskSizes[1]});
+        // 34.3
+
+        hotrod.driver[2].placeOn({diskSize: diskSizes[2]});
+        // 34.3
+        hotrod.map_haproxy.placeOn({diskSize: diskSizes[3]});
+        // 5.488
+        hotrod.customer[2].placeOn({diskSize: diskSizes[2]});
+        // 3.773
+        hotrod.redis.placeOn({diskSize: diskSizes[5]});
+        // 34.0
+        hotrod.api_haproxy.placeOn({diskSize: diskSizes[2]});
+        // 5.488
+        // 131
 
         hotrod.api[2].placeOn({diskSize: diskSizes[4]});
         // 70.0
-        hotrod.route[0].placeOn({diskSize: diskSizes[4]});
-        // 46.0
-        hotrod.route[1].placeOn({diskSize: diskSizes[4]});
-        // 46.0
-        // 131
-
-        hotrod.redis.placeOn({diskSize: diskSizes[5]});
-        // 34.0
         hotrod.route[2].placeOn({diskSize: diskSizes[5]});
         // 46.0
+        hotrod.ingress.placeOn({diskSize: diskSizes[0]});
+        // 23
         // 126
- 
     } 
 }
 
